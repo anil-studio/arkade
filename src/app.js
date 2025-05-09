22052fa269158d46b90ccb2afae70c80c8b5de20
anil-studio <anil.soilih@gmail.com>
2025-05-09 21:28:00 +0200

    Init main btn

--- src/app.js
+++ src/app.js
@@ -15,11 +15,13 @@ import _scroll from './utils/scroll'
 import Faq from './components/faq'
 import initHomeFeatures from './components/homeFeatures'
 import initHomeHero from './components/homeHero'
+import initMainBtn from './components/mainBtn'
 
 export class App {
   static {
     const homeHero = initHomeHero()
     const homeFeatures = initHomeFeatures()
     const homeFaq = new Faq(document.querySelector('.h-faq__list'))
+    const mainBtn = initMainBtn()
   }
 }
---
+++ src/components/mainBtn.js
@@ -0,0 +1,56 @@
+import gsap from '../utils/gsap'
+import { MOTION_CONFIG, ScrollTrigger } from '../utils/gsap'
+
+export default function initMainBtn() {
+  const btns = document.querySelectorAll('[data-btn="primary"]')
+  if (btns.length === 0) return
+
+  btns.forEach((btn) => {
+    const inner = btn.querySelector('.btn-inner')
+    const icon = {
+      top: btn.querySelector('.icon-top'),
+      middle: btn.querySelector('.icon-middle'),
+      bottom: btn.querySelector('.icon-bottom'),
+    }
+
+    const tl = gsap.timeline({ paused: true })
+
+    tl.to(inner, {
+      y: '0.375em',
+    })
+      .to(
+        [icon.top, icon.bottom],
+        {
+          xPercent: 100,
+          duration: MOTION_CONFIG.durationS,
+        },
+        '0.1'
+      )
+      .to(icon.top, {
+        yPercent: 100,
+        duration: MOTION_CONFIG.durationS,
+      })
+      .to(
+        icon.bottom,
+        {
+          yPercent: -100,
+          duration: MOTION_CONFIG.durationS,
+        },
+        '<'
+      )
+      .to([icon.top, icon.middle, icon.bottom], {
+        autoAlpha: 0,
+        repeat: 4,
+        //yoyo: true,
+        duration: MOTION_CONFIG.durationM,
+      })
+
+    btn.addEventListener('mouseenter', () => {
+      tl.play()
+    })
+
+    btn.addEventListener('mouseleave', () => {
+      tl.timeScale(1.5).reverse()
+    })
+  })
+}
